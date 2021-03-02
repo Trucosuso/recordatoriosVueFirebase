5b22e1c123ea9618b12607ac6d4fb84406b6acaf
Jesús L. Pinzolas <jesus@trucosuso.com>
2021-03-02 21:03:35 +0100

    Removed storage

--- src/db.js
+++ src/db.js
@@ -7,8 +7,6 @@ export const db = firebase
   .initializeApp(firebaseConfig)
   .firestore()
 
-export const storage = firebase.storage();
-
 // Export types that exists in Firestore
 // This is not always necessary, but it's used in other examples
 const { Timestamp, GeoPoint } = firebase.firestore
